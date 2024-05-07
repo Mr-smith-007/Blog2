efea7ce66157f897424383de445570430d744934
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-05-07 14:51:39 +0300

    тестовые пользователи

--- Blog2/DAL/Models/Comment.cs
+++ Blog2/DAL/Models/Comment.cs
@@ -6,9 +6,7 @@
 
         public string Title { get; set; }
         public string Body { get; set; }
-
         public string Author { get; set; }
-
         public Guid PostId { get; set; }
         public Guid AuthorId { get; set; }
         public string realAuthorName { get; set; }
