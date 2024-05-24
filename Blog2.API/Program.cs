417457d1b1af9ac057c1678fdc383e7942a28515
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-05-24 11:43:06 +0300

    Репозитории, контекст

--- Blog2.API/Program.cs
+++ Blog2.API/Program.cs
@@ -1,3 +1,7 @@
+using Blog2.API.Data.Models;
+using Blog2.API.Data.Models.Response;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +9,50 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var filepath = Path.Combine(AppContext.BaseDirectory, "Blog2.API.xml");
+    c.IncludeXmlComments(filepath);
+});
+
+// Connect DataBase
+string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<BlogDbContext>(options => options.UseSqlServer(connection))
+    .AddIdentity<User, Role>(opts =>
+    {
+        opts.Password.RequiredLength = 5;
+        opts.Password.RequireNonAlphanumeric = false;
+        opts.Password.RequireLowercase = false;
+        opts.Password.RequireUppercase = false;
+        opts.Password.RequireDigit = false;
+    })
+    .AddEntityFrameworkStores<BlogDbContext>();
+
+// Services AddSingletons/Transient
+//builder.Services
+//    .AddSingleton(mapper)
+//    .AddTransient<ICommentRepository, CommentRepository>()
+//    .AddTransient<ITagRepository, TagRepository>()
+//    .AddTransient<IPostRepository, PostRepository>()
+//    .AddTransient<IAccountService, AccountService>()
+//    .AddTransient<ICommentService, CommentService>()
+//    .AddTransient<IPostService, PostService>()
+//    .AddTransient<ITagService, TagService>()
+//    .AddTransient<IRoleService, RoleService>();
+
+builder.Services.AddAuthentication(optionts => optionts.DefaultScheme = "Cookies")
+               .AddCookie("Cookies", options =>
+               {
+                   options.Events = new Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationEvents
+                   {
+                       OnRedirectToLogin = redirectContext =>
+                       {
+                           redirectContext.HttpContext.Response.StatusCode = 401;
+                           return Task.CompletedTask;
+                       }
+                   };
+               });
+
 
 var app = builder.Build();
 
@@ -18,6 +65,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
